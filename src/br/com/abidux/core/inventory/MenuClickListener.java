32fcf532809e3259071514b55f217111ea1694c5
mabidux <70872559+mabidux@users.noreply.github.com>
2021-06-28 08:29:44 -0400

    Add files via upload

---
+++ src/br/com/abidux/core/inventory/MenuClickListener.java
@@ -0,0 +1,21 @@
+package br.com.abidux.core.inventory;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+public class MenuClickListener implements Listener {
+	
+	@EventHandler
+	void click(InventoryClickEvent event) {
+		for (Menu menu : Menu.menus) {
+			if (event.getClickedInventory() != null && event.getClickedInventory().getTitle().equals(menu.name)) {
+				event.setCancelled(true);
+				if (menu instanceof ClickableMenu) {
+					((ClickableMenu) menu).onClick(event);
+				}
+			}
+		}
+	}
+	
+}
